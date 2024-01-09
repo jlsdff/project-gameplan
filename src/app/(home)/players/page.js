7ec16e19c5471642739172a54403d08fd91053e5
Julius Duff <8215455@ntc.edu.ph>
2024-01-09 10:21:45 +0800

    changes

--- src/app/(home)/players/page.js
+++ src/app/(home)/players/page.js
@@ -1,10 +1,8 @@
 import React from "react";
-
+import UnderConstruction from "@/components/ui/underConstruction";
 
 export default function Players(){
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <p>Players Page</p>
-        </main>
+        <UnderConstruction />
     )
 }
