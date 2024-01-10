02cfec179efe2764bf567a07ad6068b34b6f872d
Julius Duff <8215455@ntc.edu.ph>
2024-01-11 02:43:42 +0800

    changes

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -9,7 +9,6 @@ import "firebase/compat/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// TODO: Store these values in .env.local
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
