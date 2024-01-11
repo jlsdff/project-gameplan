60e001842b102f5c6cd52617e2eaa4d342eed235
Julius Duff <8215455@ntc.edu.ph>
2024-01-12 06:35:07 +0800

    update

--- src/lib/firebase/getData.js
+++ src/lib/firebase/getData.js
@@ -1,22 +1,60 @@
-import { auth, firestore } from "./firebase";
+import { firestore } from "./firebase";
 
-async function getPlayers(){
-  const players = await firestore.collection("players").get()
-    .then(
-      (querySnapshot) => {
-        let players = []
-        querySnapshot.forEach((doc) => {
-          players.push(doc.data())
-        })
-        return players
-      }
-    )
-    .catch(
-      (error) => {
-        console.log("Error getting documents: ", error);
-      }
-    )
-  return players
+async function getPlayers(limit) {
+  const count = limit || 10;
+  let error;
+
+  const players = await firestore
+    .collection("players")
+    .get()
+    .then((querySnapshot) => {
+      let players = [];
+      querySnapshot.forEach((doc) => {
+        players.push(doc.data());
+      });
+      return players;
+    })
+    .catch((err) => {
+      console.log("Error getting documents: ", error);
+      error = err;
+    });
+  return error ? error : players;
 }
 
-export { getPlayers }
+async function getPlayer(id) {
+  let error;
+  const player = await firestore
+    .collection("players")
+    .doc(id)
+    .get()
+    .then((doc) => {
+      return doc.data();
+    })
+    .catch((err) => {
+      console.log("Error getting document:", error);
+      error = err;
+    });
+  return error ? error : player;
+}
+
+async function getPlayersByTeam(team) {
+  let error;
+  const players = await firestore
+    .collection("players")
+    .where("team", "==", team)
+    .get()
+    .then((querySnapshot) => {
+      let players = [];
+      querySnapshot.forEach((doc) => {
+        players.push(doc.data());
+      });
+      return players;
+    })
+    .catch((err) => {
+      console.log("Error getting documents: ", error);
+      error = err;
+    });
+  return error ? error : players;
+}
+
+export { getPlayers, getPlayer };
