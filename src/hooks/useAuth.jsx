6b18398087eec585ac34a533fde280be52c34e8a
Julius Duff <8215455@ntc.edu.ph>
2024-02-17 03:11:03 +0800

    Fixed user login

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,35 +1,35 @@
 "use client"
 import { useState, useEffect } from "react";
 import { auth } from "@/lib/firebase/firebase";
-// import { useRouter } from "next/router"; // Corrected import
-import { usePathname } from "next/router"; // Corrected import
-
+import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
 
 export function useAuth() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
-  // const pathname = usePathname();
+  const pathname = usePathname();
 
   useEffect(() => {
-    // const unsubscribe = auth.onAuthStateChanged(
-    //   async (user) => {
-    //     if (user) {
-    //       setUser(user);
-    //       if(pathname === "/admin") {
-    //         router.push("/admin/dashboard");
-    //       }
-    //     } else {
-    //       setUser(null);
-    //       router.push("/admin");
-    //     }
-    //     setLoading(false);
-    //   }  
-    // );
+    const handleAuthStateChanged = async (user) => {
+      if (user) {
+        setUser(user);
+        if (pathname === "/admin") {
+          router.push("/admin/dashboard");
+        }
+      } else {
+        setUser(null);
+        router.push("/admin");
+      }
+      setLoading(false);
+    };
 
-    // Clean up function
-    // return () => unsubscribe();
-  }, []); // Added dependencies
-  // }, [router, pathname]); // Added dependencies
+    const unsubscribe = auth.onAuthStateChanged(handleAuthStateChanged);
+
+    return () => {
+      unsubscribe();
+    };
+  }, [pathname, router]);
+
+  return { user, loading };
 }
