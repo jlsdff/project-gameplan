b076791e3f0a9c80e4226a9d844f2f9de25d428c
Julius Duff <8215455@ntc.edu.ph>
2024-02-17 03:26:45 +0800

    test

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -9,14 +9,24 @@ import "firebase/compat/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
+// let app;
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAA3j1F-HPOE8iVu1P-IHAsoSg9Ff5hbIA",
+  authDomain: "the-project-gameplan.firebaseapp.com",
+  projectId: "the-project-gameplan",
+  storageBucket: "the-project-gameplan.appspot.com",
+  messagingSenderId: "1060600103566",
+  appId: "1:1060600103566:web:1322262655eff59c47ffdf",
+  measurementId: "G-QRY63GX8DG",
 };
 let app;
 
