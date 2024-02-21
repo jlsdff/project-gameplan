a8d58a520d452bf884d55989c1900cd765da7623
Julius Duff <8215455@ntc.edu.ph>
2024-02-21 18:48:52 +0800

    changes

--- src/helpers/players/getPlayers.js
+++ src/helpers/players/getPlayers.js
@@ -16,3 +16,21 @@ export async function getAllPlayers() {
 export async function getPlayerById(id) {
   return await firestore.collection("players").doc(id).get();
 }
+
+export async function getPlayerByLikeName(name) {
+  return await firestore
+    .collection("players")
+    .orderBy("lastname")
+    .startAt(name)
+    .endAt(name + "\uf8ff")
+    .get();
+}
+
+export async function updatePlayer(id, data){
+  return await firestore.collection("players").doc(id).update(data);
+}
+
+export async function deletePlayer(id){
+  return await firestore.collection("players").doc(id).delete();
+}
+
