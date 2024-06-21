c8f431c0b8d9eaf1cd857c42bfb33f2393e1a38c
julius <8215455@ntc.edu.ph>
2024-06-21 17:51:22 +0800

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  eslint: {
+    // Ignore during builds, use with caution
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
