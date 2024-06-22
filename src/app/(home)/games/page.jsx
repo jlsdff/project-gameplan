042dcecb9b273c2c32a62e2118495d4b86aaa70e
julius <8215455@ntc.edu.ph>
2024-06-22 09:11:41 +0800

    changes

--- src/app/(home)/games/page.jsx
+++ src/app/(home)/games/page.jsx
@@ -1,95 +1,16 @@
 import React from "react";
-// import { Avatar, Link } from "@nextui-org/react";
-// import { firestore } from "@/lib/firebase/firebase";
-// import { getGamesByPage } from "@/utils/gamesAPI";
-// import GamesTable from "@/components/client/games/gamesTable";
-// import GamePagination from "@/components/client/games/gamePagination";
 import GamesPage from "@/components/client/games/gamesPage";
+import UnderMaintainance from "@/components/ui/underMaintainance";
 
 export const metadata = {
   title: "Games",
   description: "Recent games played by teams",
 };
 
-// const getData = async (searchParams) => {
-//   const page = searchParams.page || 1;
-//   const name = searchParams.name || null;
-
-//   if (name) {
-//     // Implement search by game name
-//   }
-
-//   let games = await getGamesByPage(page - 1, 5).then((snapshot) =>
-//     snapshot.docs.map((doc) => ({
-//       ...doc.data(),
-//       id: doc.id,
-//       time: doc.data().time.toDate(),
-//     }))
-//   );
-//   let teams = games.map(async (game) => {
-//     const teamA = await firestore
-//       .collection("teams")
-//       .doc(game.teamA.id)
-//       .get()
-//       .then((doc) => ({ id: doc.id, ...doc.data() }));
-
-//     const teamB = await firestore
-//       .collection("teams")
-//       .doc(game.teamB.id)
-//       .get()
-//       .then((doc) => ({ id: doc.id, ...doc.data() }));
-
-//     return {
-//       ...game,
-//       teamA: { ...game.teamA, data: teamA },
-//       teamB: { ...game.teamB, data: teamB },
-//     };
-//   });
-//   games = await Promise.all(teams);
-
-//   const leagues = games.map(async (game) => {
-//     return await firestore
-//       .collection("leagues")
-//       .doc(game.leagueId)
-//       .get()
-//       .then((doc) => ({ ...game, league: doc.data() }));
-//   });
-
-//   games = await Promise.all(leagues);
-//   const count = await firestore
-//     .collection("counters")
-//     .doc("games")
-//     .get()
-//     .then((doc) => doc.data().size);
-
-//   return [[...games], count];
-// };
-
 export default async function Page({ searchParams }) {
-  // const [games, count] = await getData(searchParams);
-  // console.log(games);
 
   return (
-    <GamesPage />
-    // <main>
-    //   <div className="px-8 mt-4 mb-2 sm:px-16 sm:mt-8">
-    //     <h1 className="text-xl font-bold sm:text-2xl">Games</h1>
-    //   </div>
-
-    //   <section>
-    //     {/* Search bar */}
-    //     <div></div>
-    //     {/* Data Display for games */}
-    //     <div className="px-8 sm:px-16">
-    //       <GamesTable games={games} />
-    //     </div>
-    //     <div className="flex items-center justify-center my-2">
-    //       <GamePagination
-    //         total={Math.ceil(count / 5)}
-    //         initialPage={searchParams.page || 1}
-    //       />
-    //     </div>
-    //   </section>
-    // </main>
+    <UnderMaintainance />
+    // <GamesPage />
   );
 }
---
+++ src/components/ui/underMaintainance.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Image } from "@nextui-org/react";
+
+export default function UnderMaintainance() {
+  return (
+    <main className="flex items-center justify-center w-full min-h-screen">
+      <section className="flex flex-col items-center gap-4">
+        <div className="w-full sm:w-1/2">
+          <Image className="object-fit" src="/underMaintainance.svg" alt="" />
+        </div>
+        <div className="space-y-2 text-center">
+          <h1 className="text-4xl font-bold">Under Maintainance</h1>
+          <p className="text-2xl font-semibold tracking-wide"> We&apos;ll be right back as soon as we can. </p>
+        </div>
+      </section>
+    </main>
+  );
+}
