3afb7c834014e7e20727b175bd52d5f60d54b942
Julius Duff <8215455@ntc.edu.ph>
2024-11-01 06:23:58 +0800

    test

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -12,13 +12,13 @@ import "firebase/compat/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAA3j1F-HPOE8iVu1P-IHAsoSg9Ff5hbIA", 
+  authDomain: "the-project-gameplan.firebaseapp.com",
+  projectId: "the-project-gameplan",
+  storageBucket: "the-project-gameplan.appspot.com",
+  messagingSenderId: "1060600103566",
+  appId: "1:1060600103566:web:1322262655eff59c47ffdf",
+  measurementId: "G-QRY63GX8DG",
 };
 
 let app;
--- src/utils/algolia/algolia.js
+++ src/utils/algolia/algolia.js
@@ -1,7 +1,7 @@
 import { searchClient } from "@algolia/client-search";
 
-const app_id = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID
-const search_api_key = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY
+const app_id = "E0U8VX4KVQ"
+const search_api_key = "c8bc8457576a9c30c322b5ebcbc59182"
 
 const client = searchClient(app_id, search_api_key)
 
