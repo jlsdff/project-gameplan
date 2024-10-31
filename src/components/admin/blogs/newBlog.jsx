6f2f93a0a019cc5117c78fc3ae99a0993c9129f4
Julius Duff <8215455@ntc.edu.ph>
2024-11-01 05:39:06 +0800

    Refactor import path for Editor component in newBlog.jsx

--- src/components/admin/blogs/newBlog.jsx
+++ src/components/admin/blogs/newBlog.jsx
@@ -14,7 +14,7 @@ import {
   Image,
 } from "@nextui-org/react";
 import { createBlog, getBlogById, updateBlog } from "@/utils/blogsAPI";
-import Editor from "@/components/ui/editor/editorJs";
+import Editor from "@/components/ui/editor/editor";
 import { useRouter, useSearchParams } from "next/navigation";
 import { updateLeague } from "@/utils/leagueAPI";
 
