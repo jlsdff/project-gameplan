eb5073395615090878d28f520e8fdc5f7ac13048
Julius Duff <8215455@ntc.edu.ph>
2024-11-03 11:45:26 +0800

    reset admin game page

--- src/app/admin/dashboard/games/new/page.jsx
+++ src/app/admin/dashboard/games/new/page.jsx
@@ -1,10 +1,14 @@
+import React from "react";
+// import NewGame from "@/components/admin/games/newGame";
+import NewGame from "@/components/admin/games/new/NewGame";
+import NewGamePage from "@/components/admin/games/new/NewGamePage";
 
-import React from 'react'
-import NewGame from '@/components/admin/games/newGame'
-export default function NewGamePage() {
+export default function Page({id}) {
+
+  {/* <NewGame /> */}
   return (
-    <main className='min-h-screen p-8 sm:p-16'>
-      <NewGame />
-    </main>
-  )
+      <>
+        <NewGamePage id={id} />
+      </>
+  );
 }
