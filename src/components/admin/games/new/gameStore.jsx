5a9d371df1d33891b022c9522dcd24d6bffb3c7c
julius <8215455@ntc.edu.ph>
2024-11-04 16:21:17 +0800

    changes

--- src/components/admin/games/new/gameStore.jsx
+++ src/components/admin/games/new/gameStore.jsx
@@ -1,47 +1,60 @@
 import { create } from "zustand";
 import { getParticipatingTeamsData } from "@/utils/leagueAPI";
-import { parseDate, getLocalTimeZone } from "@internationalized/date"
-import { useDateFormatter } from "@react-aria/i18n"
+import { parseDate, getLocalTimeZone } from "@internationalized/date";
+import { useDateFormatter } from "@react-aria/i18n";
 import { getPlayers } from "@/utils/teamAPI";
 
-const getTodayFormatted = (_date=null) => {
+const getTodayFormatted = (_date = null) => {
   const date = _date ? _date : new Date();
   const yyyy = date.getFullYear();
-  const mm = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-  const dd = String(date.getDate()).padStart(2, '0');
+  const mm = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const dd = String(date.getDate()).padStart(2, "0");
   return `${yyyy}-${mm}-${dd}`;
 };
+
+const reducer = (state, action) => {};
 
 export const useNewGameStore = create((set) => ({
   league: null,
   teamParticipants: [],
   teamA: null,
   teamAPlayers: [],
-  teamAStats: [],
   teamB: null,
   teamBPlayers: [],
-  teamBStats: [],
+  stats: [
+    {
+      teamA: [],
+      teamB: [],
+    },
+  ],
   date: parseDate(getTodayFormatted()),
   number: null,
   lastGame: null,
   setLeague: async (league) => {
-    const teamParticipants = await getParticipatingTeamsData(league.participatingTeams)
-      .then( docs => docs.map( doc => ({ id: doc.id, ref: doc, ...doc.data() }) ) )
-    console.log("teamParticipants: ", teamParticipants)
+    const teamParticipants = await getParticipatingTeamsData(
+      league.participatingTeams
+    ).then((docs) =>
+      docs.map((doc) => ({ id: doc.id, ref: doc, ...doc.data() }))
+    );
+    console.log("teamParticipants: ", teamParticipants);
     set({ league, teamParticipants });
   },
   setTeamA: async (team) => {
-    let players = team.players
-    players = await getPlayers(players)
-      .then( docs => docs.map( doc => ({ id: doc.id, ref: doc, ...doc.data() }) ) )
-    set({ teamA: team, teamAPlayers: players })
+    let players = team.players;
+    players = await getPlayers(players).then((docs) =>
+      docs.map((doc) => ({ id: doc.id, ref: doc, ...doc.data() }))
+    );
+    set({ teamA: team, teamAPlayers: players });
   },
   setTeamB: async (team) => {
-    let players = team.players
-    players = await getPlayers(players)
-      .then( docs => docs.map( doc => ({ id: doc.id, ref: doc, ...doc.data() }) ) )
-    set({ teamB: team, teamBPlayers: players })
+    let players = team.players;
+    players = await getPlayers(players).then((docs) =>
+      docs.map((doc) => ({ id: doc.id, ref: doc, ...doc.data() }))
+    );
+    set({ teamB: team, teamBPlayers: players });
   },
   setDate: (date) => set({ date }),
   setGameNumber: (gameNumber) => set({ gameNumber }),
+  edit: (id) => {},
+  statDispatch: (action) => set((state) => reducer(state, action)),
 }));
